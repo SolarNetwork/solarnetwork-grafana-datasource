8efd99608d80fe26428c445cf7bd77b175d0d45e
Matt Magoffin <matt@solarnetwork.net>
2025-07-22 11:59:30 +1200

    Rename backend plugin.

--- pkg/main.go
+++ pkg/main.go
@@ -5,11 +5,11 @@ import (
 
 	"github.com/grafana/grafana-plugin-sdk-go/backend/datasource"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
-	"revolve-energy-solarnetwork-datasource-backend/pkg/plugin"
+	"solarnetwork-datasource-backend/pkg/plugin"
 )
 
 func main() {
-	if err := datasource.Manage("revolve-energy-solarnetwork-datasource-backend", plugin.NewSolarNetworkDatasource, datasource.ManageOpts{}); err != nil {
+	if err := datasource.Manage("solarnetwork-datasource-backend", plugin.NewSolarNetworkDatasource, datasource.ManageOpts{}); err != nil {
 		log.DefaultLogger.Error(err.Error())
 		os.Exit(1)
 	}
